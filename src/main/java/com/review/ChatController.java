fb618d500984159fa678212fc690daa4ec104b8c
aziky <pcm230304@gmail.com>
2025-09-28 16:14:54 +0700

    add cors

--- src/main/java/com/review/ChatController.java
+++ src/main/java/com/review/ChatController.java
@@ -1,6 +1,7 @@
 package com.review;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.Map;
 
 @RestController
 @Slf4j
+@CrossOrigin(origins = "*")
 public class ChatController {
 
     private final ChatService chatService;
