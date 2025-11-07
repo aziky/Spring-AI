c7dfc177806bffb42ca08ac02504247c18cea62d
aziky <pcm230304@gmail.com>
2025-11-07 09:22:39 +0700

    add mln document

--- src/main/java/com/review/ChatController.java
+++ src/main/java/com/review/ChatController.java
@@ -1,7 +1,6 @@
 package com.review;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
